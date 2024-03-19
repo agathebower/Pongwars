290bd18a001d664770c22d9318231481169a1bfe
agathebower <50291894+agathebower@users.noreply.github.com>
2024-03-19 19:58:36 +0100

    Update PongWars.ino

--- PongWars.ino
+++ PongWars.ino
@@ -10,8 +10,8 @@
 
 /* Definitions */
 
-uint16_t MYWHITE=0x0001; // placeholder
-uint16_t MYBLACK=0xFFFF; // placeholder
+uint16_t MYWHITE=0x0001;
+uint16_t MYBLACK=0xFFFF;
 const uint16_t DAY_COLOR = MYWHITE;
 const uint16_t DAY_BALL_COLOR = MYBLACK;
 const uint16_t NIGHT_COLOR = MYBLACK;
@@ -26,8 +26,7 @@ const int BALL_HEIGHT = 2;
 int dayScore = 0;
 int nightScore = 0;
 
-// error: size of array 'squares' is not an integral constant-expression // when using functions/methods to get values for canvas_width -- because variable length array would be defined... (VLA)
-const int32_t canvas_width = 135; // tested via displayTests project
+const int32_t canvas_width = 135;
 const int32_t canvas_height = 240;
 const float numSquaresX = canvas_width / SQUARE_SIZE;
 const float numSquaresY = canvas_height / SQUARE_SIZE;
@@ -44,11 +43,6 @@ struct BALL {
   uint16_t ballColor;
 };
 
-/*orig
-BALL balls[] = {
-  {canvas_width / 4 , canvas_height / 2, 8, -8, NIGHT_COLOR, NIGHT_BALL_COLOR},
-  {(canvas_width / 4) * 3, canvas_height / 2, -8, 8, DAY_COLOR, DAY_BALL_COLOR},
-};*/
 BALL balls[] = {
   {canvas_width / 4 , canvas_height / 2, 4, -4, NIGHT_COLOR, 0x03E0},
   {(canvas_width / 4) * 3, canvas_height / 2, -4, 4, DAY_COLOR, 0x867D},
@@ -74,7 +68,6 @@ void intro()
 
 void drawBall(BALL *ball, LovyanGFX* gfx)
 {
-  //gfx->fillRect(ball->x, ball->y, BALL_WIDTH, BALL_HEIGHT, ball->ballColor);
   StickCP2.Display.fillCircle(ball->x, ball->y, 2, ball->ballColor);
 }
 
@@ -113,27 +106,9 @@ void checkBoundaryCollision(BALL *ball)
 
 void addRandomness(BALL *ball) 
 {
-/*
-  ball->dx += random() * 0.01 - 0.005;
-  ball->dy += random() * 0.01 - 0.005;
-
-  // Limit the speed of the ball
-  ball->dx = min(max(ball->dx, -MAX_SPEED), MAX_SPEED);
-  ball->dy = min(max(ball->dy, -MAX_SPEED), MAX_SPEED);
-
-  // Make sure the ball always maintains a minimum speed
-  if (abs(ball->dx) < MIN_SPEED)
-  {
-    ball->dx = ball->dx > 0 ? MIN_SPEED : -MIN_SPEED;
-  }
-
-  if (abs(ball->dy) < MIN_SPEED)
-  {
-    ball->dy = ball->dy > 0 ? MIN_SPEED : -MIN_SPEED;
-  }*/
-
   ball->dx += random(MIN_SPEED, MAX_SPEED) * 0.01 - 0.005;
   ball->dy += random(MIN_SPEED, MAX_SPEED) * 0.01 - 0.005;
+
   // Make sure the ball always maintains a minimum speed
   if (abs(ball->dx) < MIN_SPEED)
   {
@@ -154,7 +129,6 @@ void draw(LovyanGFX* gfx)
 
   drawSquares(&StickCP2.Display);
 
-  //StickCP2.Display.setCursor((canvas_width / 4) * 3, canvas_height / 2);
   StickCP2.Display.setCursor(90, 100);
   StickCP2.Display.printf("%i", dayScore);
   StickCP2.Display.setCursor(20, 100);
@@ -176,8 +150,7 @@ void draw(LovyanGFX* gfx)
 void checkSquareCollision(BALL *ball) 
 {
   // Check multiple points around the ball's circumference
-//  for (int angle = 0; angle < PI * 2; angle += PI / 4)
-  for (int angle = 0; angle < PI * 2; angle += PI / 2) // not sure why this works yet -- but with '2' it works...
+  for (int angle = 0; angle < PI * 2; angle += PI / 2)
   {
     float checkX = ball->x + cos(angle) * HALF_SQUARE_SIZE;
     float checkY = ball->y + sin(angle) * HALF_SQUARE_SIZE;
@@ -185,7 +158,6 @@ void checkSquareCollision(BALL *ball)
     int i = floor(checkX / SQUARE_SIZE);
     int j = floor(checkY / SQUARE_SIZE);
 
-//    if (i >= 0 && i < numSquaresX && j >= 0 && j < numSquaresY) 
     if (i >= 0 && i < nsx && j >= 0 && j < nsy) 
     {
       if (squares[i][j] != ball->reverseColor) 
@@ -206,7 +178,6 @@ void checkSquareCollision(BALL *ball)
   }
 }
 
-
 /* MAIN Methods */
 
 void setup() 
@@ -215,7 +186,6 @@ void setup()
   StickCP2.begin(cfg);
   StickCP2.Display.setRotation(2);
   StickCP2.Display.setTextColor(RED);
-  //StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
   StickCP2.Display.setTextSize(1);
 
   int h = StickCP2.Display.width();
@@ -226,13 +196,12 @@ void setup()
   intro();
 
   StickCP2.Display.clear();
+
   // Populate the fields, one half day, one half night
   for (int i = 0; i < nsx; i++)
   {
-    // squares[i] = [];
     for (int j = 0; j < nsy; j++)
     {
-      //squares[i][j] = i < numSquaresX / 2 ? DAY_COLOR : NIGHT_COLOR;
       squares[i][j] = i < nsx / 2 ? NIGHT_COLOR : DAY_COLOR;
     }
   }
